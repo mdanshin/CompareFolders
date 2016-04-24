cbf6dc02cfd7cda4506b04b2714c12c33dc1d7e2
Mikhail Danshin <mdanshin@gmail.com>
2016-04-24 23:12:35 +0300

    Add new class. Fix 1 bug. Some code changes.

--- CompareFolders/CompareFolders.cs
+++ CompareFolders/CompareFolders.cs
@@ -12,6 +12,7 @@ namespace CompareFolders
     {
         #region Declaring variable
         const string _title = "Сравнение папок";
+        const int _minFileSize = 1; //Size in bytes
         const long _maxFileSize = 104857600; //Size in bytes
 
         static int _fileCount1;
@@ -27,9 +28,9 @@ namespace CompareFolders
         {
             Console.Title = _title;
             Console.Clear();
-            PrintHeader();
+            Printer.PrintHeader();
 
-            if (args.Length >= 2)
+            if (args.Length >= 2 && args[0] != "/?")
             {
                 #region Start
                 if (args.Length >= 3 && args[2] != null && args[2] == "/quite")
@@ -38,7 +39,7 @@ namespace CompareFolders
                 }
                 else if (args.Length >= 3 && args[2] != null && args[2] != "/quite")
                 {
-                    PrintErrorr();
+                    Printer.PrintErrorr();
                     Environment.Exit(0);
                 }
                 if (args.Length >= 4 && args[3] != null && args[3] == "/print")
@@ -47,7 +48,7 @@ namespace CompareFolders
                 }
                 else if (args.Length >= 4 && args[3] != null && args[3] != "/print")
                 {
-                    PrintErrorr();
+                    Printer.PrintErrorr();
                     Environment.Exit(0);
                 }
 
@@ -59,11 +60,12 @@ namespace CompareFolders
                 {
                     var fileSize = new FileInfo(file).Length;
 
-                    if (fileSize < _maxFileSize)
+                    if (fileSize < _maxFileSize & fileSize >= _minFileSize) //TODO:Подумать как оптимизировать, чтобы не делать двойную проверку
                     {
+                        //TODO: Дублирование кода!
                         _fileCount1++;
                         FillFileTabe(file);
-                        PrintProgress();
+                        Printer.PrintProgress(_fileCount1.ToString(), _fileCount2.ToString(), file);
                     }
                     else
                     {
@@ -76,12 +78,16 @@ namespace CompareFolders
                 {
                     var fileSize = new FileInfo(file).Length;
 
-                    if (fileSize < _maxFileSize)
+                    if (fileSize < _maxFileSize & fileSize >= _minFileSize) //TODO:Подумать как оптимизировать, чтобы не делать двойную проверку
                     {
                         //TODO: Дублирование кода!
                         _fileCount2++;
                         FillFileTabe(file);
-                        PrintProgress();
+                        Printer.PrintProgress(_fileCount1.ToString(), _fileCount2.ToString(), file);
+                    }
+                    else
+                    {
+                        _skupedFiles.Add(file);
                     }
                 }
 
@@ -93,8 +99,12 @@ namespace CompareFolders
                 Console.WriteLine("Всего обработано {0} файлов\n", (_fileCount1 + _fileCount2).ToString());
                 Console.WriteLine("Найдено уникальных файлов {0}\n", tableOfFiles.Count);
                 Console.WriteLine("Время выполнения (чч:мм:сс:мс): {0}:{1}:{2}:{3}\n", watch.Elapsed.Hours, watch.Elapsed.Minutes, watch.Elapsed.Seconds, watch.Elapsed.Milliseconds);
-                Console.WriteLine("Пропущены следующие файлы:");
-                Console.WriteLine(_skupedFiles);
+                Console.WriteLine("Пропущено {0} файл(ов):", _skupedFiles.Count);
+                for (int i = 0; i < _skupedFiles.Count; i++)
+                {
+                    Console.WriteLine(_skupedFiles[i]);
+                }
+                
 
                 if (tableOfFiles.Count != 0 && _quite && _print)
                 {
@@ -110,7 +120,7 @@ namespace CompareFolders
                 if (tableOfFiles.Count != 0 && !_quite)
                 {
                     Console.WriteLine("Вывести список уникальных файлов? (y/N)");
-                    if (Console.ReadKey().Key.ToString() == "Y")
+                    if (Console.ReadKey(true).Key.ToString() == "Y")
                     {
                         //Вывод на экран получившегося уникального списка значений
                         foreach (DictionaryEntry entry in tableOfFiles)
@@ -125,11 +135,11 @@ namespace CompareFolders
             {
                 if (args.Length != 0 && args[0] == "/?")
                 {
-                    PrintHelp();
+                    Printer.PrintHelp();
                 }
                 else
                 {
-                    PrintErrorr();
+                    Printer.PrintErrorr();
                 }
             }
 
@@ -202,43 +212,6 @@ namespace CompareFolders
                 //Возможно стоит организовать какой-то счетчик
                 return null;
             }
-        }
-        static void PrintProgress()
-        {
-            Console.CursorTop = 3;
-            Console.WriteLine("В первой папке обработано {0} файлов", _fileCount1.ToString());
-            Console.CursorTop = 4;
-            Console.WriteLine("Во второй папке обработано {0} файлов\n", _fileCount2.ToString());
-        }
-        static void PrintHeader()
-        {
-            Console.WriteLine("Сравнение папок [версия " + GetAssemblyVersion() + "]");
-            Console.WriteLine("Автор: Михаил Даньшин, (с) 2015");
-        }
-        static void PrintErrorr()
-        {
-            Console.WriteLine("\nОШИБКА: Аргументы заданы неверно\n");
-            Console.WriteLine("Пример использования: CompareFolders.exe c:\\folder1 c:\\folder2 [/quite] [/print]\n");
-            Console.WriteLine("Для вывода справки используйте ключ /?");
-        }
-        static void PrintHelp()
-        {
-            Console.WriteLine("\nCOMPAREFOLDERS c:\\folder1 c:\\folder2 [/quite] [/print]\n");
-            Console.WriteLine("/quite\tНе запрашивать вывод списка уникальных файлов на экран");
-            Console.WriteLine("/print\tВывести список уникальных файлов на экран (используется с ключем /quite)");
-            Console.WriteLine("Для сравнения файлов используется алгоритм хеширования MD5.\n");
-            Console.WriteLine("В следующей версии:\n");
-            Console.WriteLine("/file:filename[.txt]\tВывести список уникальных файлов в файл \t\t\t\t\t(при использовании /quite и /print)\n");
-            Console.WriteLine(">>Функция удаления неуникальных файлов");
-            Console.WriteLine(">>Функция копирования уникальных файлов в третью папку");
-
-        }
-        static string GetAssemblyVersion()
-        {
-            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
-            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
-            string version = fvi.FileVersion;
-            return version;
         }
     }
 }
