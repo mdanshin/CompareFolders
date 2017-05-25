6a1565dc4c9c03c6f07746ba65f5904314831d4a
Danshin Mikhail <mdanshin@gmail.com>
2017-05-25 11:05:14 +0300

    Some change. Unfortunately I already forgot what exactly I was changing.

--- CompareFolders/CompareFolders.cs
+++ CompareFolders/CompareFolders.cs
@@ -13,14 +13,14 @@ namespace CompareFolders
         #region Declaring variable
 
         private const string Title      = "Сравнение папок";
-        private const int MinFileSize   = 1; //Size in bytes
-        private const long MaxFileSize  = 104857600; //Size in bytes
+        //private const int MinFileSize = 1; //Size in bytes
+        //private const long MaxFileSize = 104857600; //Size in bytes
 
-        private static Hashtable TableOfFiles { get; } = new Hashtable();
-        private static List<string> SkipedFiles { get; } = new List<string>();
+        //private static Hashtable TableOfFiles { get; } = new Hashtable();
+        //private static List<string> SkipedFiles { get; } = new List<string>();
 
-        private static int FileCount1 { get; set; }
-        private static int FileCount2 { get; set; }
+        //private static int FileCount1 { get; set; }
+        //private static int FileCount2 { get; set; }
 
         private static bool Quite { get; set; }
         private static bool Print { get; set; }
@@ -32,55 +32,51 @@ namespace CompareFolders
             ConsoleReady();
             CommandPromptArgsCheck(args);
 
-            #region Start
+            //#region Start
+            //HashFilesTables fileTable = new HashFilesTables(args[0], args[1]);
 
-            var watch = new Stopwatch();
-            watch.Start();
+            ////Получаем список файлов из директории a1 и добавляем их в таблицу
+            //foreach (var file in GetFiles(args[0]))
+            //{
+            //    var fileSize = new FileInfo(file).Length;
 
-            FilesTables fileTable = new FilesTables(args[0]);
+            //    if (fileSize < MaxFileSize & fileSize >= MinFileSize) //TODO:Подумать как оптимизировать, чтобы не делать двойную проверку
+            //    {
+            //        //TODO: Дублирование кода!
+            //        FileCount1++;
+            //        FillFileTabe(file);
+            //        Printer.PrintProgress(FileCount1.ToString(), FileCount2.ToString(), file);
+            //    }
+            //    else
+            //    {
+            //        SkipedFiles.Add(file);
+            //    }
+            //}
 
-            //Получаем список файлов из директории a1 и добавляем их в таблицу
-            foreach (var file in GetFiles(args[0]))
-            {
-                var fileSize = new FileInfo(file).Length;
+            ////Получаем список файлов из директории a2 и добавляем в таблицу tableOfFiles только те файлы, которых там нет
+            //foreach (var file in GetFiles(args[1]))
+            //{
+            //    var fileSize = new FileInfo(file).Length;
 
-                if (fileSize < MaxFileSize & fileSize >= MinFileSize) //TODO:Подумать как оптимизировать, чтобы не делать двойную проверку
-                {
-                    //TODO: Дублирование кода!
-                    FileCount1++;
-                    FillFileTabe(file);
-                    Printer.PrintProgress(FileCount1.ToString(), FileCount2.ToString(), file);
-                }
-                else
-                {
-                    SkipedFiles.Add(file);
-                }
-            }
+            //    if (fileSize < MaxFileSize & fileSize >= MinFileSize) //TODO:Подумать как оптимизировать, чтобы не делать двойную проверку
+            //    {
+            //        //TODO: Дублирование кода!
+            //        FileCount2++;
+            //        FillFileTabe(file);
+            //        Printer.PrintProgress(FileCount1.ToString(), FileCount2.ToString(), file);
+            //    }
+            //    else
+            //    {
+            //        SkipedFiles.Add(file);
+            //    }
+            //}
+            //#endregion
 
-            //Получаем список файлов из директории a2 и добавляем в таблицу tableOfFiles только те файлы, которых там нет
-            foreach (var file in GetFiles(args[1]))
-            {
-                var fileSize = new FileInfo(file).Length;
+            //Result();
 
-                if (fileSize < MaxFileSize & fileSize >= MinFileSize) //TODO:Подумать как оптимизировать, чтобы не делать двойную проверку
-                {
-                    //TODO: Дублирование кода!
-                    FileCount2++;
-                    FillFileTabe(file);
-                    Printer.PrintProgress(FileCount1.ToString(), FileCount2.ToString(), file);
-                }
-                else
-                {
-                    SkipedFiles.Add(file);
-                }
-            }
-
-            watch.Stop();
-            #endregion
-
-            Result(watch);
-
-            PrintResult();
+            //PrintResult();
+            HashFilesTables hashFilesTables = new HashFilesTables(args);
+            hashFilesTables.Begin();
         }
 
         private static void ConsoleReady()
@@ -145,113 +141,113 @@ namespace CompareFolders
             }
         }
 
-        private static void Result(Stopwatch watch)
-        {
-            Console.CursorLeft = 0;
-            Console.WriteLine($"Всего обработано {FileCount1 + FileCount2} файлов\n");
-            Console.WriteLine($"Найдено уникальных файлов {TableOfFiles.Count}\n");
-            Console.WriteLine($"Время выполнения (чч:мм:сс:мс): {watch.Elapsed.Hours}:{watch.Elapsed.Minutes}:{watch.Elapsed.Seconds}:{watch.Elapsed.Milliseconds}\n");
-            Console.WriteLine($"Пропущено {SkipedFiles.Count} файл(ов):");
+        //private static void Result()
+        //{
+        //    Console.CursorLeft = 0;
+        //    Console.WriteLine($"Всего обработано {FileCount1 + FileCount2} файлов\n");
+        //    Console.WriteLine($"Найдено уникальных файлов {TableOfFiles.Count}\n");
+        //    Console.WriteLine($"Время выполнения (чч:мм:сс:мс):");
+        //    Console.WriteLine($"Пропущено {SkipedFiles.Count} файл(ов):");
 
-            foreach (var t in SkipedFiles)
-            {
-                Console.WriteLine(t);
-            }
+        //    foreach (var t in SkipedFiles)
+        //    {
+        //        Console.WriteLine(t);
+        //    }
 
 
-            if (TableOfFiles.Count != 0 && Quite && Print)
-            {
-                //Вывод на экран получившегося уникального списка значений
-                foreach (DictionaryEntry entry in TableOfFiles)
-                {
-                    Console.WriteLine($"{entry.Key}");
-                }
-            }
-        }
+        //    if (TableOfFiles.Count != 0 && Quite && Print)
+        //    {
+        //        //Вывод на экран получившегося уникального списка значений
+        //        foreach (DictionaryEntry entry in TableOfFiles)
+        //        {
+        //            Console.WriteLine($"{entry.Key}");
+        //        }
+        //    }
+        //}
 
-        private static void PrintResult()
-        {
-            if (TableOfFiles.Count == 0 || Quite) return;
-            {
-                Console.WriteLine("Вывести список уникальных файлов? (y/N)");
-                if (Console.ReadKey(true).Key.ToString() != "Y") return;
-                //Вывод на экран получившегося уникального списка значений
-                foreach (DictionaryEntry entry in TableOfFiles)
-                {
-                    Console.WriteLine($"{entry.Key}");
-                }
-            }
-        }
+        //private static void PrintResult()
+        //{
+        //    if (TableOfFiles.Count == 0 || Quite) return;
+        //    {
+        //        Console.WriteLine("Вывести список уникальных файлов? (y/N)");
+        //        if (Console.ReadKey(true).Key.ToString() != "Y") return;
+        //        //Вывод на экран получившегося уникального списка значений
+        //        foreach (DictionaryEntry entry in TableOfFiles)
+        //        {
+        //            Console.WriteLine($"{entry.Key}");
+        //        }
+        //    }
+        //}
 
-        private static IEnumerable<string> GetFiles(string path)
-        {
-            var queue = new Queue<string>();
-            queue.Enqueue(path);
-            while (queue.Count > 0)
-            {
-                path = queue.Dequeue();
-                string[] files = null;
+        //private static IEnumerable<string> GetFiles(string path)
+        //{
+        //    var queue = new Queue<string>();
+        //    queue.Enqueue(path);
+        //    while (queue.Count > 0)
+        //    {
+        //        path = queue.Dequeue();
+        //        string[] files = null;
 
-                try
-                {
-                    foreach (var subDir in Directory.GetDirectories(path))
-                    {
-                        queue.Enqueue(subDir);
-                    }
+        //        try
+        //        {
+        //            foreach (var subDir in Directory.GetDirectories(path))
+        //            {
+        //                queue.Enqueue(subDir);
+        //            }
 
-                    files = Directory.GetFiles(path);
-                }
-                catch (UnauthorizedAccessException)
-                {
-                    SkipedFiles.Add(path);
-                }
-                catch (Exception ex)
-                {
-                    Console.Error.WriteLine("\n" + ex.Message);
-                    if (ex.InnerException != null)
-                        Console.Error.WriteLine("\n" + ex.InnerException);
-                    Environment.Exit(0);
-                }
+        //            files = Directory.GetFiles(path);
+        //        }
+        //        catch (UnauthorizedAccessException)
+        //        {
+        //            SkipedFiles.Add(path);
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            Console.Error.WriteLine("\n" + ex.Message);
+        //            if (ex.InnerException != null)
+        //                Console.Error.WriteLine("\n" + ex.InnerException);
+        //            Environment.Exit(0);
+        //        }
 
-                if (files == null) continue;
-                foreach (var t in files)
-                {
-                    yield return t;
-                }
-            }
-        }
+        //        if (files == null) continue;
+        //        foreach (var t in files)
+        //        {
+        //            yield return t;
+        //        }
+        //    }
+        //}
 
-        private static void FillFileTabe(string file)
-        {
-            var hash = GetFileHash(file);
-            var val = TableOfFiles.ContainsValue(hash);
+        //private static void FillFileTabe(string file)
+        //{
+        //    var hash = GetFileHash(file);
+        //    var val = TableOfFiles.ContainsValue(hash);
 
-            if (!val) TableOfFiles.Add(file, hash);
-        }
+        //    if (!val) TableOfFiles.Add(file, hash);
+        //}
 
-        //Нужно следить за тем, чтобы эта функция вызывалась не более одного раза.
-        //Это влияет на производительность.
-        //TODO: Возможно стоит организовать SingleTone?
-        private static string GetFileHash(string filename)
-        {
-            try
-            {
-                using (var md5 = MD5.Create())
-                {
-                    using (var stream = File.OpenRead(filename))
-                    {
-                        return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", "").ToLower();
-                    }
-                }
-            }
-            catch(Exception ex) when (ex is IOException || ex is UnauthorizedAccessException)
-            {
-                //TODO: С точки зрения ООП неправильно размещать в этом блоке логику. Переделать.
-                SkipedFiles.Add(filename);
-                //TODO: Тут есть проблема. Если в дальнейшем встретится несколько значений null, то они будут восприняты как одинаковые.
-                //Возможно стоит организовать какой-то счетчик
-                return null;
-            }
-        }
+        ////Нужно следить за тем, чтобы эта функция вызывалась не более одного раза.
+        ////Это влияет на производительность.
+        ////TODO: Возможно стоит организовать SingleTone?
+        //private static string GetFileHash(string filename)
+        //{
+        //    try
+        //    {
+        //        using (var md5 = MD5.Create())
+        //        {
+        //            using (var stream = File.OpenRead(filename))
+        //            {
+        //                return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", "").ToLower();
+        //            }
+        //        }
+        //    }
+        //    catch(Exception ex) when (ex is IOException || ex is UnauthorizedAccessException)
+        //    {
+        //        //TODO: С точки зрения ООП неправильно размещать в этом блоке логику. Переделать.
+        //        SkipedFiles.Add(filename);
+        //        //TODO: Тут есть проблема. Если в дальнейшем встретится несколько значений null, то они будут восприняты как одинаковые.
+        //        //Возможно стоит организовать какой-то счетчик
+        //        return null;
+        //    }
+        //}
     }
 }
---
+++ CompareFolders/HashFilesTables.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace CompareFolders
+{
+    public class HashFilesTables
+    {
+        private const int MinFileSize = 1; //Size in bytes
+        private const long MaxFileSize = 104857600; //Size in bytes
+
+        private string PathFirst { get; }
+        private string PathSecond { get; }
+        private List<FileEntity> FilesListHashFirst { get; set; }
+        private List<FileEntity> FilesListHashSecond { get; set; }
+        private List<FileEntity> FilesListSkipped { get; set; }
+
+        public HashFilesTables(string[] args)
+        {
+            PathFirst = args[0];
+            PathSecond = args[1];
+        }
+
+        public void Begin(string output="console")
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<FileEntity> GetFilesList(string kindFilesList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<FileEntity> GetUnique()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void FillTables(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string GetFileHash(string path)
+        {
+            try
+            {
+                using (var md5 = MD5.Create())
+                {
+                    using (var stream = File.OpenRead(path))
+                    {
+                        return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", "").ToLower();
+                    }
+                }
+            }
+            catch (Exception ex) when (ex is IOException || ex is UnauthorizedAccessException)
+            {
+                return null;
+            }
+        }
+    }
+}
